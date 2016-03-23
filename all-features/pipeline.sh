14c30a23d1643e50c991b1d9ebbbbea320cce6c8
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2016-03-23 10:28:21 -0700

    Move all-features to its own directory

---
+++ all-features/pipeline.sh
@@ -0,0 +1,10 @@
+# Run from this directory
+
+SERVERS=../../construct/integrate/neo4j/servers.py
+python $SERVERS --write servers.json
+python $SERVERS --start-all
+
+jupyter nbconvert --execute metapaths.ipynb --inplace --ExecutePreprocessor.timeout=-1
+jupyter nbconvert --execute extract.ipynb --inplace --ExecutePreprocessor.timeout=-1
+jupyter nbconvert --execute performance.ipynb --inplace --ExecutePreprocessor.timeout=-1
+jupyter nbconvert --execute pyvisualize.ipynb --inplace --ExecutePreprocessor.timeout=-1
