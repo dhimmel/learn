7644f5fad03a930de9fb946e5096dc5a0b519002
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2016-03-28 20:18:25 -0700

    Compute features for all metapaths
    Compute features for all metapaths with length <= 4, but on only a subset of
    negatives. Compute for the unpermuted hetnet and 4 permuted hetnets.
    
    Based on the hetnets created in
    dhimmel/integrate@1229536c6d2146c4cae97f045cf8cbdd272420f6

--- all-features/pipeline.sh
+++ all-features/pipeline.sh
@@ -5,6 +5,7 @@ python $SERVERS --write servers.json
 python $SERVERS --start-all
 
 jupyter nbconvert --execute metapaths.ipynb --inplace --ExecutePreprocessor.timeout=-1
+jupyter nbconvert --execute partition.ipynb --inplace --ExecutePreprocessor.timeout=-1
 jupyter nbconvert --execute extract.ipynb --inplace --ExecutePreprocessor.timeout=-1
 jupyter nbconvert --execute performance.ipynb --inplace --ExecutePreprocessor.timeout=-1
 jupyter nbconvert --execute pyvisualize.ipynb --inplace --ExecutePreprocessor.timeout=-1
